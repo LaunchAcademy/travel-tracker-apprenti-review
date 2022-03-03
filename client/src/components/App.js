fe93700d32a968d5931a1477a90ec592dc4c16dc
Stephen Fang <safang99@gmail.com>
2022-03-03 16:01:57 -0500

    removed finished code from starter

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,59 +1,15 @@
-import React, { useState } from 'react';
-import PlaceTile from "./PlaceTile"
+import React from "react"
 
-const App = (props) => {
-  // debugger
-  
-  const [selectedPlaceId, setSelectedPlaceId] = useState(null)
-
-  const placesList = props.data.places.map((place) => {
-    // debugger
-    // <li key={place.id}>{place.name}</li>
-    const handleClick = () => {
-      // debugger
-      setSelectedPlaceId(place.id)
-    }
-
-    let placeStyle = ""
-    if (selectedPlaceId === place.id) {
-      placeStyle = "done"
-    }
-
-    return (
-      <PlaceTile 
-        key={place.id}
-        placeName={place.name}
-        handleClick={handleClick}
-        placeStyle={placeStyle}
-      />
-    )
-  })
-  // debugger
-
-  let message = ""
-  
-  // debugger
-  if (selectedPlaceId === props.data.favoritePlaceId) {
-    // debugger
-    message = (
-      <div>
-        What a beauty!
-      </div>
-    )
-  }
-
+const App = () => {
   return (
     <div id="wishlist-div">
       <div className="grid-container">
         <div className="small-12 text-center">
           <h3>Wanderlust Wishlist</h3>
-          {placesList}
-
-          {message}
         </div>
       </div>
     </div>
   )
 }
 
-export default App;
+export default App
